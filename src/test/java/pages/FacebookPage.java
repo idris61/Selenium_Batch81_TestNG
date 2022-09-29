c75241824ef4f3d2663812d833255d699a6259be
İdris <idris.gemici61@gmail.com>
2022-09-29 23:47:45 +0300

    29.09.22

--- src/test/java/pages/FacebookPage.java
+++ src/test/java/pages/FacebookPage.java
@@ -1,20 +1,25 @@
 package pages;
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.Factory;
 import utilities.Driver;
-
-import java.awt.print.PageFormat;
 
 public class FacebookPage {
 
     public FacebookPage() {
+
         PageFactory.initElements(Driver.getDriver(), this);
 
     }
+
     @FindBy(xpath = "//*[@id='email']")
     public WebElement email;
+    @FindBy(xpath = "//*[@id='pass']")
+    public WebElement sifre;
+    @FindBy(xpath = "//*[@class='_9ay7']")
+    public WebElement girisYapilmadi;
+    @FindBy(xpath = "//*[@class='_42ft _4jy0 _6lth _4jy6 _4jy1 selected _51sy']")
+    public WebElement login;
+
 
 }
